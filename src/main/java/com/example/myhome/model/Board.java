e1f5f96d4fe31c86ef2a285845b34355a7e3f6bf
leyosu23 <leyosu23@gmail.com>
2022-01-25 09:58:41 -0500

    Update submitting a form with validator, data binding

--- src/main/java/com/example/myhome/model/Board.java
+++ src/main/java/com/example/myhome/model/Board.java
@@ -1,11 +1,12 @@
 package com.example.myhome.model;
 
 import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,6 +14,8 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull
+    @Size(min=2, max=30, message = "Titile should be between 2 to 30 letters")
     private String title;
     private String content;
 
